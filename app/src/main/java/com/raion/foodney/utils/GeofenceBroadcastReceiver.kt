0c8ffa521e88a5bb5acdbf3a942d4fbdbc426309
ibrahimss02 <tosaritonosambataheri@gmail.com>
2022-10-26 11:49:17 +0700

    Initial commit

---
+++ app/src/main/java/com/raion/foodney/utils/GeofenceBroadcastReceiver.kt
@@ -0,0 +1,11 @@
+package com.raion.foodney.utils
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class GeofenceBroadcastReceiver: BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("Not yet implemented")
+    }
+}
