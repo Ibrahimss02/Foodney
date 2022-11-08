b0dd67e70b6653d914bbce0ba407201593fbd4da
ibrahimss02 <tosaritonosambataheri@gmail.com>
2022-11-08 15:04:45 +0700

    fixing bottom nav & add icon

--- app/src/main/java/com/raion/foodney/MainActivity.kt
+++ app/src/main/java/com/raion/foodney/MainActivity.kt
@@ -2,10 +2,22 @@ package com.raion.foodney
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.raion.foodney.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.itemIconTintList = null
+        val fragmentManager = supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
+        val navController = fragmentManager.navController
+        binding.bottomNavBar.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/raion/foodney/ui/welcomeScreens/signUp/SignUpFragment.kt
+++ app/src/main/java/com/raion/foodney/ui/welcomeScreens/signUp/SignUpFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.raion.foodney.R
+import com.raion.foodney.databinding.FragmentSignUpBinding
+
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    ): View {
+        binding = FragmentSignUpBinding.inflate(layoutInflater)
+
+        binding.tvMasuk.setOnClickListener {
+            findNavController().navigate(SignUpFragmentDirections.actionSignUpFragmentToSignInFragment())
+        }
+
+
+
+        return binding.root
     }
 }
