63fa66e45a39fba3c29dc98dd855600ed6bd68ac
ibrahimss02 <tosaritonosambataheri@gmail.com>
2022-11-10 09:51:05 +0700

    merging warung and mission models

--- app/src/main/java/com/raion/foodney/models/Mission.kt
+++ app/src/main/java/com/raion/foodney/models/Mission.kt
@@ -1,11 +1,17 @@
 package com.raion.foodney.models
 
+import com.google.android.gms.maps.model.LatLng
+
 data class Mission(
     val id: String,
     val name: String,
-    val warung: Warung,
     val coinReward: Int,
     var distance: Int,
+    val address: String,
+    val rating: Float,
+    val latLng: LatLng,
+    val photoUrls: List<String>,
+    val reviews: List<Review>
 )
 
 object MissionDummy {
