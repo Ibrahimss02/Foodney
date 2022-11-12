3bd09bae8e610144343cf47f682af7fbf2f7ff6e
fanggadewangga <fanggadewangga@gmail.com>
2022-11-12 10:11:16 +0700

    add: leaderboard layout

--- app/src/main/java/com/raion/foodney/models/Coupon.kt
+++ app/src/main/java/com/raion/foodney/models/Coupon.kt
@@ -5,52 +5,52 @@ data class Coupon(
     val name: String,
     val couponDesc: String,
     val cost: Int,
-    val image: String
+    val image: String?
 )
 
 object CouponDummy {
-    val couponData = arrayListOf<Coupon>(
+    val couponData = arrayListOf(
         // TODO: INSERT DUMMY DATA HERE
         Coupon(
             "Coupon_01",
             "Mie Bakar Celaket",
             "Beli makanan apa saja dan dapatkan potongan sebesar Rp 10.000,00",
-            "10000",
+            10000,
             null
         ),
         Coupon(
             "Coupon_02",
             "Pecel Sambal Tumpang Bu Djarot",
             "Beli makanan apa saja dan dapatkan potongan sebesar Rp 5.000,00",
-            "5000",
+            5000,
             null
         ),
         Coupon(
             "Coupon_03",
             "Es Setrup \" Slamet \"",
             "Beli makanan apa saja dan dapatkan potongan sebesar Rp 7.500,00",
-            "7500",
+            7500,
             null
-        )
+        ),
         Coupon(
             "Coupon_04",
             "Lalapan Cak Midi",
             "Beli makanan apa saja dan dapatkan potongan sebesar Rp 8.000,00",
-            "8000",
+            8000,
             null
-        )
+        ),
         Coupon(
             "Coupon_05",
             "Lalapan Belut Fresh",
             "Beli makanan apa saja dan dapatkan potongan sebesar Rp 15.000,00",
-            "15000",
+            15000,
             null
-        )
+        ),
         Coupon(
             "Coupon_06",
             "Tahu Campur Dan Tahu Telor Cak Roon",
             "Beli makanan apa saja dan dapatkan potongan sebesar Rp 10.000,00",
-            "10000",
+            10000,
             null
         )
     )
