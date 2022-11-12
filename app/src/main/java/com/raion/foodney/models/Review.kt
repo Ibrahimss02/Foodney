f6657df374196c56db56edf686fe3d9fc243d0be
Melin Febriani <106047611+melinfebriani@users.noreply.github.com>
2022-11-12 11:31:56 +0700

    Update Review.kt

--- app/src/main/java/com/raion/foodney/models/Review.kt
+++ app/src/main/java/com/raion/foodney/models/Review.kt
@@ -10,5 +10,41 @@ data class Review(
 object ReviewDummy{
     val reviewData = arrayListOf<Review>(
         // TODO: INSERT DUMMY DATA HERE
+        Review(
+            "Review_01",
+            "putin",
+            "Makanannya enak, porsinya banyak, dan murah",
+            5
+        ),
+        Review(
+            "Review_02",
+            "arnold",
+            "Harganya murah, rasanya sesuai harga",
+            4
+        ),
+        Review(
+            "Review_03",
+            "renatta",
+            "Makanannya enak banget, pelayanannya juga cepat. Ada banyak menu yang bisa coba",
+            4.5
+        ),
+        Review(
+            "Review_04",
+            "quinn",
+            "Ini ketiga kalinya saya datang ke tempat ini. Lokasinya strategis, banyak pilihan menu",
+            5
+        ),
+        Review(
+            "Review_05",
+            "reynold",
+            "Tempatnya lumayan nyaman, makanannya juga enak enak",
+            4
+        ),
+        Review(
+            "Review_06",
+            "zelenskyy",
+            "Rekomendasi banget dehh, harganya terjangkau dan enak",
+            4.5
+        )
     )
 }
