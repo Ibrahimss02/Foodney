a098b7a0458a358d12eb4226adde58124b1913f8
Melin Febriani <106047611+melinfebriani@users.noreply.github.com>
2022-11-13 20:57:03 +0700

    Update UserLeaderboard.kt

--- app/src/main/java/com/raion/foodney/models/UserLeaderboard.kt
+++ app/src/main/java/com/raion/foodney/models/UserLeaderboard.kt
@@ -8,5 +8,55 @@ data class UserLeaderboard(
 object LeaderboardDummy {
     val leaderboardData = listOf<LeaderboardDummy>(
         /* TODO : INSERT DUMMY HERE */
+        UserLeaderboard (
+            "arnold",
+            50,
+            R.drawable.arnold
+        ),
+        UserLeaderboard (
+            "renatta",
+            49,
+            R.drawable.renatta
+        ),
+        UserLeaderboard (
+            "quinn",
+            48,
+            R.drawable.quinn
+        ),
+        UserLeaderboard (
+            "putin",
+            47,
+            R.drawable.putin
+        ),
+        UserLeaderboard (
+            "reynold",
+            46,
+            R.drawable.reynold
+        ),
+        UserLeaderboard (
+            "juna",
+            45,
+            R.drawable.juna
+        ),
+        UserLeaderboard (
+            "zelenskyy",
+            44,
+            R.drawable.zelenskyy
+        ),
+        UserLeaderboard (
+            "spongebob",
+            43,
+            R.drawable.spongebob
+        ),
+        UserLeaderboard (
+            "remy",
+            42,
+            R.drawable.remy
+        ),
+        UserLeaderboard (
+            "kimjongun",
+            41,
+            R.drawable.kimjongun
+        )
     )
 }
